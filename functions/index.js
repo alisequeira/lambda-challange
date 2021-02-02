f20bab8a94120f28b745e1a020287d6905e70f8f
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-02-02 13:10:12 -0500

    fix

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
+const DB = require('../database/db');
 
 
 module.exports.handler = async (event, context) => {
-    import DB from '../database/db';
     const db = new DB();
     const allEntries = await db.getAll();
     const length = allEntries.length;
