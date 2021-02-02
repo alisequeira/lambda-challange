c758ae5eddfd10bd91f7dce07e1c29e87350963a
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-02-02 13:08:02 -0500

    .

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
-import DB from '../database/db';
+
 
 module.exports.handler = async (event, context) => {
+    import DB from '../database/db';
     const db = new DB();
     const allEntries = await db.getAll();
     const length = allEntries.length;
