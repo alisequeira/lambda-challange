12b99c1a97dce36f4be78b739c369017f1825cbd
Ali Sequeira <alisequeira.la.240@gmail.com>
2021-02-02 13:06:45 -0500

    fix

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,12 @@
+import DB from '../database/db';
+
 module.exports.handler = async (event, context) => {
+    const db = new DB();
+    const allEntries = await db.getAll();
+    const length = allEntries.length;
+    console.log(allEntries);
     return {
-        statusCode = 200,
-        body: JSON.stringify({ message: "hello" })
+        statusCode: 200,
+        body: JSON.stringify({ message: 'Data success', data: allEntries, length: length })
     }
 };
